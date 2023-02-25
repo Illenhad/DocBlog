dc4b288a8650c17d041d2510aa0d0d4869f4eba9
Pierre Legrand <pierrelegrand.pro@gmail.com>
2023-02-25 19:02:28 +0100

    [ADD] Added the first template.

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
-def index(requests):
-    return HttpResponse('<h1>Blog</h1>')
+def index(request: HttpRequest) -> HttpResponse:
+    """
+    Initial view of the Blog app.
+    
+    :param requests: This is the request object that is sent to the server
+    :return: The index template file is being returned.
+    """
+    return render(request=request, template_name='blog/index.html')
